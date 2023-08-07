331febfb3a95c3e0d021925b57e01b53bdab52b3
Alexis Garcia <azoreno2@gmail.com>
2023-08-07 15:50:53 -0400

    Change cache dependency

--- src/middlewares/requestCache.ts
+++
@@ -1,8 +0,0 @@
-import expeditious from 'express-expeditious'
-
-const cacheOptions: expeditious.ExpeditiousOptions = {
-  namespace: 'expresscache',
-  defaultTtl: '5 minutes'
-}
-
-export const cacheHandler = expeditious(cacheOptions)
--- src/routes/usersRoutes/user.routes.ts
+++ src/routes/usersRoutes/user.routes.ts
@@ -1,15 +1,16 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
+import apicache from 'apicache'
 import { loginUser, refreshToken, registerUser } from '../../controllers/users'
 import { validateJWTMiddleware, verifyIfUserExist, verifyIfUsernameExist } from '../../middlewares'
 import { validateZod } from '../../middlewares/validateSchemas'
 import { loginSchema, registerSchema } from '../../schemas'
-import { cacheHandler } from '../../middlewares/requestCache'
+const cache = apicache.middleware
 
 const userRouter = Router()
 
-userRouter.post('/register', validateZod(registerSchema), verifyIfUserExist, verifyIfUsernameExist, cacheHandler, registerUser)
-userRouter.post('/login', validateZod(loginSchema), verifyIfUserExist, cacheHandler, loginUser)
-userRouter.get('/refresh', validateJWTMiddleware, cacheHandler, refreshToken)
+userRouter.post('/register', cache('1 minute'), validateZod(registerSchema), verifyIfUserExist, verifyIfUsernameExist, registerUser)
+userRouter.post('/login', cache('1 minute'), validateZod(loginSchema), verifyIfUserExist, loginUser)
+userRouter.get('/refresh', cache('1 minute'), validateJWTMiddleware, refreshToken)
 
 export { userRouter }
